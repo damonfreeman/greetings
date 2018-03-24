012a7b230b67645e2baa0f1f3b50d44d650f12f3
Damon Freeman <damonefreeman@gmail.com>
2018-03-23 20:25:30 -0500

    Add all files and first working site

---
+++ app.js
@@ -0,0 +1,19 @@
+var inputBox1 = document.getElementById("input-box1");
+var inputBox2 = document.getElementById("input-box2");
+var textOutputBox = document.getElementById("text-output");
+var submitButton = document.getElementById("submit-button");
+var resetButton = document.getElementById("reset-button");
+var greatTextBox = textOutputBox.innerHTML;
+
+submitButton.addEventListener("click", function() {
+  var userText1 = inputBox1.value.toUpperCase();
+  var userText2 = inputBox2.value;
+  if(userText1 === "DAMON") {
+    textOutputBox.innerHTML += "<p>" + userText + " is a practicing Software Engineer." + "</p>";
+  } else {
+    for(var i = 0; i < userText2 ; i++) {
+      textOutputBox.innerHTML += "<p>" + "Hello, " + userText1 + " it is very nice to meet you." + "</p>";
+    }
+  }
+})
+
